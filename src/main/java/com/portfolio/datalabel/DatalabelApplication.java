6ca43285ae9b1b45f9ccc6832dcd32f6d5e9766f
YeaBinShin <toplondon08@gmail.com>
2026-03-25 22:37:30 +0900

    feat: Spring boot 환경설정 추가, Docker Compose (MySQL, Redis) 추가

---
+++ src/main/java/com/portfolio/datalabel/DatalabelApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.datalabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatalabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/datalabel/DatalabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.datalabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatalabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
